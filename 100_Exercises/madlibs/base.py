17a4125fa7a08f32c319285548c72902fcf75a35
Yejin Kwon <YKwon@gusd.net>
2023-10-19 09:46:32 -0700

    adding madlibs exercise

---
+++ 100_Exercises/madlibs/base.py
@@ -0,0 +1,21 @@
+# Create at least 10 variables here. You should have a mix of verbs, adjectives and nouns.
+
+
+
+
+# Using print statements and your variables, create a mad libs style story.
+# This means that if someone were to change the values of the variables above and ran your code, 
+# they'd see a completely different story.
+
+
+
+
+
+# Here's a (short) example if you need one
+adjective1 = "shocking"
+noun1 = "potato"
+word1 = "ahoy"
+past_adjective = "terrified"
+print("I was walking to the store when suddenly a " + adjective1 + " " + noun1 + " flew past my face")
+print("The " + noun1 + " said " + word1)
+print("At the sound of its voice I was " + past_adjective)
