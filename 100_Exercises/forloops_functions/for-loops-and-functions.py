a5c31fb10c2c9638d2ce5c5b6b1981b44312b21e
Yejin Kwon <YKwon@gusd.net>
2023-11-09 08:14:51 -0800

    new worksheet

---
+++ 100_Exercises/forloops_functions/for-loops-and-functions.py
@@ -0,0 +1,41 @@
+## QUESTION 1
+# Ask the user for some number. 
+# Then, write a function to print all the even numbers from 0 up to that number. 
+# This function should take in the user input as a parameter.
+# For example, if the user types in 5, the function should print out 0, 2, 4. (treat 0 as an even number)
+# Your code should use a for loop.
+
+## WRITE CODE HERE
+
+
+
+
+## QUESTION 2
+# Do what you did in Question 1 but for odd numbers instead.
+# For example, if the user types in 5, the function should print out 1, 3, 5.
+# Your code should use a for loop.
+
+## WRITE CODE HERE
+
+
+
+
+## QUESTION 3
+# Ask the user for some number.
+# Write a function that prints out the sum from 1 to that number.
+# For example, if the user types in 5, the function should print out 15.
+# Your code should use a for loop.
+
+## WRITE CODE HERE
+
+
+## Challenge: QUESTION 4
+# Using a for loop print out the following pattern:
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+## WRITE CODE HERE
+
