c7d286ad6d7849126df1ae5a62726eb56bc8d37f
Yejin Kwon <YKwon@gusd.net>
2023-11-28 09:52:44 -0800

    adding new project

---
+++ 100_Exercises/to-do-list/app.py
@@ -0,0 +1,6 @@
+todo_list = []
+
+while True: # this line of code makes it so that the code inside will keep running (forever!)
+    operation = input("What would you like to do? ('add, remove, print'): ")
+
+    # the rest of your code should go inside here. 
